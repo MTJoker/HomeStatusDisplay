cb6a24226dde4e5114f010b6567044614273a987
MTJoker <mail@bernd-schubart.de>
2017-02-12 19:56:42 +0100

    add some documentation

--- Heartbeat.h
+++ Heartbeat.h
@@ -1,6 +1,11 @@
 #ifndef _HEARTBEAT_H
 #define _HEARTBEAT_H
 
+/**
+ * Simple class to realize a "heartbeat" - that is a led which blinks in
+ * a given interval to visualize the system is still working.
+ */
+
 class Heartbeat
 {
 public:
