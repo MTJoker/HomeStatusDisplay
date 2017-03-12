9eaeadb3880bbcc923003fa8c7ad0c947ddac999
MTJoker <mail@bernd-schubart.de>
2017-03-12 23:06:40 +0100

    v0.4

--- HomeStatusDisplay.ino
+++ HomeStatusDisplay.ino
@@ -1,6 +1,6 @@
 #include "HomeStatusDisplay.h"
 
-static const char* VERSION = "0.3";
+static const char* VERSION = "0.4";
 static const char* IDENTIFIER = "HomeStatusDisplay";
 
 HomeStatusDisplay display;
