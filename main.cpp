169bbd81b6ba4e1cffaf2261bf1d0883c9ba983a
MTJoker <mail@bernd-schubart.de>
2026-01-02 09:13:12 +0100

    increase version

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
-#include <Arduino.h>
 #include "HomeStatusDisplay.hpp"
+#include <Arduino.h>
 
-static const char* VERSION = "0.6_dev";
+static const char* VERSION = "0.7_dev";
 static const char* IDENTIFIER = "HomeStatusDisplay";
 
 HomeStatusDisplay display;
 
-void setup() 
-{ 
-  display.begin(VERSION, IDENTIFIER);
+void setup()
+{
+    display.begin(VERSION, IDENTIFIER);
 }
 
-void loop() 
-{     
-  display.work();
+void loop()
+{
+    display.work();
 }
-
