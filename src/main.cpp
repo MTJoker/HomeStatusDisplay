ed9b632986a12cc54598f5c577cafa77cc2b7e63
MTJoker <mail@bernd-schubart.de>
2026-01-05 12:36:16 +0100

    bump version to 1.0.0 beta

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "HomeStatusDisplay.hpp"
 #include <Arduino.h>
 
-constexpr const char* VERSION = "0.7_dev";
+constexpr const char* VERSION = "1.0.0 beta";
 constexpr const char* IDENTIFIER = "HomeStatusDisplay";
 
 HomeStatusDisplay display;
